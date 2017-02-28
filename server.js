b59c214d2cbf3ecc2e3a1cfc939c2a6cada81b27
Jessica <jessisena@gmail.com>
2017-02-28 09:31:37 +0100

    prep heroku 2

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use(function(req, res, next){
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port ' +port);
+  console.log('Express server is up on port ' +PORT);
 });
